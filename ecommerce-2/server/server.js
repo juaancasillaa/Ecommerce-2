404500037ff16abb7e9a34151bfa39d4ddae4abc
juaancasillaa <juanacasilla16@outlook.com>
2024-07-30 13:53:18 -0400

    DONE

--- ecommerce-2/server/server.js
+++ ecommerce-2/server/server.js
@@ -5,7 +5,10 @@ const cors = require('cors');
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: '*',
+}));
+
 app.use(express.json()); // Add this middleware to parse JSON
 
 const PORT = 5050;
